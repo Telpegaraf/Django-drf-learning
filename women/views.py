b7dee17fd61b5a1c7b2f1b8aa5871db237859c61
Telpegaraf <telpegaraf@gmail.com>
2023-07-22 11:40:32 +0400

    add router

--- women/views.py
+++ women/views.py
@@ -1,11 +1,32 @@
-from rest_framework import generics, viewsets
-from women.models import Women
+from rest_framework import generics, viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+from women.models import Women, Category
 from women.serializers import WomenSerializer
 
 
-class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
+class WomenViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+    #queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+
+        if not pk:
+            return Women.objects.all()[:3]
+
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=False)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': [cats.name]})
 
 
 # class WomenAPIList(generics.ListCreateAPIView):
